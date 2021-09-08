cb41553a50da5c8a464c009b51ada4464859309a
Antonija K <antonija.kasum@gmail.com>
2021-09-08 23:23:19 +0300

    break timer

--- src/views/Workout.vue
+++ src/views/Workout.vue
@@ -3,28 +3,35 @@
     <h2 class="heading">Workout {{ type }}</h2>
 
     <ul class="wo">
-        <li v-for="step in workout" :key="step.title" class="step">
-            <h3 class="title">
-               {{ step.title }}
-            </h3>
+      <!-------------------------- S T E P -------------------------->
+      <li v-for="step in workout" :key="step.title" class="step">
+        <h3 class="title">
+          {{ step.title }}
+        </h3>
 
-             <h4 class="time">
-                 Break 
-                 <span><i :class="'icon fas fa-clock'"></i> {{step.break}} sec</span>
-            </h4>
+        <h4 class="time">
+          Break
+          <span><i :class="'icon fas fa-clock'"></i> {{ step.break }} sec</span>
+        </h4>
 
-            <label v-for="index in step.set.req" :key="index" class="set">
-               {{step.set.rep}} <input type="checkbox"/> 
-            </label>
+        <!-------------------------- R E P S -------------------------->
+        <label v-for="index in step.set.req" :key="index + 'req'" class="set">
+          {{ step.set.rep }} <input type="checkbox" @click="startBreak(step)" />
+        </label>
 
-            <label v-for="index in step.set.opt" :key="index" class="set-opt">
-               {{step.set.rep}} <input type="checkbox"/> 
-            </label>
+        <label
+          v-for="index in step.set.opt"
+          :key="index + 'opt'"
+          class="set-opt"
+        >
+          {{ step.set.rep }} <input type="checkbox" @click="startBreak(step)" />
+        </label>
 
-            <div v-if="timerVisible" class="timer">
-                {{time}}
-            </div>
-        </li>
+        <!-------------------------- T I M E R -------------------------->
+        <div v-if="visibleTimer === step.title" class="timer">
+          {{ time }}
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -38,158 +45,184 @@ export default {
   },
   data() {
     return {
-      timerVisible: true,
+      interval: null,
+      visibleTimer: "",
       time: 0,
       type: "legs",
       legs: [
-          {
-              title: "Hack squat",
-              set: {
-                  req: 3,
-                  opt: 1,
-                  rep: 8
-              },
-              weight: 0,
-              break: 60
+        {
+          title: "Hack squat",
+          set: {
+            req: 3,
+            opt: 1,
+            rep: 8,
           },
-          {
-              title: "Hack squat",
-              set: {
-                  req: 3,
-                  opt: 1,
-                  rep: 8
-              },
-              weight: 0,
-                break: 60
+          weight: 0,
+          break: 60,
+        },
+        {
+          title: "Hip thrusts",
+          set: {
+            req: 3,
+            opt: 1,
+            rep: 8,
           },
-          {
-              title: "Hack squat",
-              set: {
-                  req: 3,
-                  opt: 1,
-                  rep: 8
-              },
-              weight: 0
-          }
+          weight: 0,
+          break: 60,
+        },
+        {
+          title: "Hack squat2",
+          set: {
+            req: 3,
+            opt: 1,
+            rep: 8,
+          },
+          weight: 0,
+        },
       ],
-      workout: null
+      workout: null,
     };
+  },
+  methods: {
+    startBreak(step) {
+      this.visibleTimer = step.title;
+      this.time = step.break;
+
+      clearInterval(this.interval);
+      this.interval = setInterval(this.runTime, 1000);
+    },
+    runTime() {
+      this.time--;
+
+      if (this.time === 0) {
+        this.visibleTimer = "";
+        clearInterval(this.interval);
+      }
+    },
   },
 };
 </script>
 
 <style scoped lang="scss">
 .workout {
-    .wo {
-        padding: 0;
-        color: white;
+  .wo {
+    padding: 0;
+    color: white;
 
-        .step {
-              list-style: none;
-              padding: 1.5rem;
-              padding-bottom: 3rem;
-              background-color: rgb(52, 52, 58);
-              position: relative;
+    .step {
+      list-style: none;
+      padding: 1.5rem;
+      padding-bottom: 3rem;
+      background-color: rgb(52, 52, 58);
+      position: relative;
 
-              &:nth-child(0n + 2){
-                background-color: transparent;
+      &:nth-child(even) {
+        background-color: transparent;
 
-                .time {
-                    background: rgb(52, 52, 58);
-                }
-            }
-
-            .title {
-                margin-bottom: .75rem;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-            }
-
-            .time {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    font-weight: normal;
-                    font-size: .9rem;
-                    margin-bottom: 1.5rem;
-                    background-color: rgb(59, 59, 66);
-                    padding: .25rem .5rem;
-                    color: black;
-                    border-radius: 3px;
-
-                    .icon {
-                        margin-right: .25rem;
-                    }
-            }
-
-            .timer {
-                position: absolute;
-                top: calc(50% - 2.5rem);
-                left: 1.5rem;
-                border: 3px solid white;
-                border-radius: 50%;
-                width: 5rem;
-                height: 5rem;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-
-
-
-            .set, .set-opt {
-                display: flex;
-                height: 2rem;
-                justify-content: flex-end;
-                align-items: center;
-                padding: 1.5rem 0;
-                width: 100%;
-                font-weight: bold;
-                font-size: 1.1rem;
-
-                input {
-                    margin-left: .75rem;
-                    visibility: hidden;
-                    position: relative;
-                    overflow: hidden;
-                    height: 1.25rem;
-                        width: 1.25rem;
-
-                    &:before {
-                        visibility: visible;
-                        content: "";
-                        height: 1rem;
-                        width: 1rem;
-                        border: 1px solid white;
-                        position: absolute;
-                        left: 0;
-                        top: 0;
-                    }
-
-                    &:checked:after {
-                        visibility: visible;
-                        content: "\2715";
-                        height: 1rem;
-                        width: 1rem;
-                        position: absolute;
-                        left: 1px;
-                        background-color: red;
-                        color: white;
-                        font-size: 1rem;
-                        line-height: .9rem;
-                        display: block;
-                        top: 1px;
-                        text-align: center;
-                    }
-                }
-            }
-
-            .set-opt {
-                color: gray;
-                border-top: 1px solid gray;
-            }
+        .time {
+          color: rgb(54, 52, 55);
         }
+      }
+
+      .title {
+        margin-bottom: 0.75rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      .time {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-weight: normal;
+        font-size: 0.9rem;
+        margin-bottom: 1.5rem;
+        border: 1px solid rgb(56, 56, 62);
+        padding: 0.25rem 0.5rem;
+        color: rgb(32, 32, 34);
+        border-radius: 3px;
+
+        .icon {
+          margin-right: 0.25rem;
+        }
+      }
+
+      .timer {
+        position: fixed;
+        top: calc(50% - 5.25rem);
+        left: calc(50% - 5.25rem);
+        border: 8px solid white;
+        border-radius: 50%;
+        width: 10.5rem;
+        height: 10.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 5rem;
+        background-color: rgba(10, 145, 75, 0.5);
+        z-index: 1;
+        transition: all .2s linear;
+    
+      }
+
+      .set,
+      .set-opt {
+        display: flex;
+        height: 2rem;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 1.5rem 0;
+        width: 100%;
+        font-weight: bold;
+        font-size: 1.1rem;
+
+        input {
+          margin-left: 0.75rem;
+          visibility: hidden;
+          position: relative;
+          overflow: hidden;
+          height: 1.25rem;
+          width: 1.25rem;
+
+          &:before {
+            visibility: visible;
+            content: "";
+            height: 1rem;
+            width: 1rem;
+            border: 1px solid white;
+            position: absolute;
+            left: 0;
+            top: 0;
+          }
+
+          &:checked:before{
+            border-color: rgb(13, 99, 40);
+          }
+
+          &:checked:after {
+            visibility: visible;
+            content: "\2715";
+            height: 1rem;
+            width: 1rem;
+            position: absolute;
+            left: 1px;
+            background-color: rgb(13, 99, 40);
+            color: white;
+            font-size: 1rem;
+            line-height: 0.9rem;
+            display: block;
+            top: 1px;
+            text-align: center;
+          }
+        }
+      }
+
+      .set-opt {
+        color: gray;
+        border-top: 1px solid gray;
+      }
     }
+  }
 }
 </style>
